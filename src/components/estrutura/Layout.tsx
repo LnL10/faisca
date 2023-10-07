4566c2a2058532a4542c35b4f0dd945c6ab678a5
Leonel <a48543@alunos.ipb.pt>
2023-10-07 16:43:53 +0100

    Update

--- src/components/estrutura/Layout.tsx
+++ src/components/estrutura/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 export default function Layout({ children }:any) {
   return (
-    <div className='flex flex-col bg-white  w-screen h-full'>
+    <div className='flex flex-col bg-white  w-full h-full'>
       <Header />
       {children}
       <Footer/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
   
   return (
     <Layout>
-      <div className="w-full">
         <div className='flex flex-col bg-white'>
             <h1 className="flex justify-center p-3 text-xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-5xl">
               <span className="text-transparent pb-2 bg-clip-text bg-gradient-to-r from-red-600 via-black to-red-600 ">
@@ -30,7 +29,6 @@ export default function Home() {
               <Faq/>
             </div>
         </div>
-      </div>
 
     </Layout>
       
