3a1be6df9254d8c9c275f951d7d3faa5810cd6c9
Leonel <a48543@alunos.ipb.pt>
2023-10-07 16:39:25 +0100

    ResponsiveMode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,20 +15,21 @@ export default function Home() {
   
   return (
     <Layout>
-      <span className='text-black'>FAISCA</span>
-      <div className='flex flex-col bg-white'>
-          <h1 className="flex justify-center p-3 text-xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-5xl">
-            <span className="text-transparent pb-2 bg-clip-text bg-gradient-to-r from-red-600 via-black to-red-600 ">
-              PROMOÇÕES
-            </span>
-          </h1>
+      <div className="w-full">
+        <div className='flex flex-col bg-white'>
+            <h1 className="flex justify-center p-3 text-xl font-extrabold text-gray-900 dark:text-white md:text-3xl lg:text-5xl">
+              <span className="text-transparent pb-2 bg-clip-text bg-gradient-to-r from-red-600 via-black to-red-600 ">
+                PROMOÇÕES
+              </span>
+            </h1>
 
 
-          <ListaProdutos produtos={produtos2} comprar={adicionarProduto}/>
-          <span className='flex justify-center text-black font-black pt-10'>PERGUNTAS FREQUENTES</span>
-          <div className=' flex justify-center pt-5 pb-10'>
-            <Faq/>
-          </div>
+            <ListaProdutos produtos={produtos2} comprar={adicionarProduto}/>
+            <span className='flex justify-center text-black font-black pt-10'>PERGUNTAS FREQUENTES</span>
+            <div className=' flex justify-center pt-5 pb-10'>
+              <Faq/>
+            </div>
+        </div>
       </div>
 
     </Layout>
